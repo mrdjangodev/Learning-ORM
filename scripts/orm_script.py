fc53dd7552d447c2664aef7802b62afc736ed05a
mrdjangodev <mirolimcoder@gmail.com>
2023-09-28 21:23:46 +0500

    filtering doctors done

--- scripts/orm_script.py
+++ scripts/orm_script.py
@@ -4,11 +4,13 @@ from pprint import pprint
 
 # from local
 from hospital.models import Patient, Doctor, Specialization
-from .utils import get_all_head_doctors
+from .utils import get_all_head_doctors, filter_doctors_by_currency_type, filter_doctors_by_is_active_status
 
 def run():
-    head_doctors = get_all_head_doctors()
-    print(f"all head doctors: \n{head_doctors}")
-    # for head_doctor in head_doctors:
-    #     print(head_doctor.first_name)
+    # head_doctors = get_all_head_doctors()
+    # print(f"all head doctors: \n{head_doctors}")
+    doctors_by_currency = filter_doctors_by_currency_type('EUr')
+    doctors_by_actie_status = filter_doctors_by_is_active_status(False)
+    print(f"doctors_by_currency: \n{doctors_by_currency} \n\
+        doctors_by_actie_status: \n{doctors_by_actie_status}")
     pprint(connection.queries)
--- scripts/utils.py
+++ scripts/utils.py
@@ -8,8 +8,23 @@ from departments.models import Department
 
 
 def get_all_head_doctors():
+    """retrieves all HeadDoctors"""
     # departments = Department.objects.select_related('head_doctor')
     # head_doctors = [department.head_doctor for department in departments]
+    
+    # commented code is not more afficient than uncommented one because
+    # it has list comperhension and it involves an additional query to retrieve 
+    # the departments and requires extra processing to extract the head doctors
     head_doctors = Doctor.objects.filter(department__isnull=False)
     return head_doctors
-    
+
+
+def filter_doctors_by_currency_type(currency_type:str):
+    """
+        available currencies : ['usd', 'uzs', 'eur']
+    """
+    return Doctor.objects.filter(salary_currency=currency_type.lower())
+
+
+def filter_doctors_by_is_active_status(is_active:bool = True):
+    return Doctor.objects.filter(is_active=is_active)
