d10cbd2b2305712634cb66d0f5baa7c7fa54b8e7
mrdjangodev <mirolimcoder@gmail.com>
2023-09-30 13:41:41 +0500

    FUNCTIONS BELONG TO beds done

--- scripts/orm_script.py
+++ scripts/orm_script.py
@@ -9,10 +9,11 @@ from hospital.models import Patient, Doctor, Specialization
 from .utils import (
     get_all_head_doctors, filter_doctors_by_currency_type, 
     filter_doctors_by_is_active_status, filter_all_appointments_by_status,
-    get_all_doctors_belong_to_department,
+    get_all_doctors_belong_to_department, filter_beds_by_availabilty
                     )
 
 def run():
+    start_time = time.time()
     # head_doctors = get_all_head_doctors()
     # print(f"all head doctors: \n{head_doctors}")
     # doctors_by_currency = filter_doctors_by_currency_type('EUr')
@@ -23,10 +24,11 @@ def run():
     # filtered_appointments = filter_all_appointments_by_status('scheduled')
     # print(f"Filtered appointments: \n{filtered_appointments}")
     
-    department = Department.objects.first()
-    start_time = time.time()
-    retrived_doctors_by_department = get_all_doctors_belong_to_department(department)
+    # department = Department.objects.first()
+    # retrived_doctors_by_department = get_all_doctors_belong_to_department(department)
+    # pprint(f"retrived_doctors_by_department: {retrived_doctors_by_department}")
+    filtered_beds = filter_beds_by_availabilty('available')
     finish_time = time.time()
+    pprint(f"{filtered_beds}")
     print(f"Runtime: {finish_time-start_time}")
-    pprint(f"retrived_doctors_by_department: {retrived_doctors_by_department}")
     pprint(connection.queries)
--- scripts/utils.py
+++ scripts/utils.py
@@ -4,7 +4,7 @@
 
 # from local
 from hospital.models import Doctor
-from departments.models import Department
+from departments.models import Department, Bed
 from events.models import Appointment
 
 def get_all_head_doctors():
@@ -41,6 +41,17 @@ def filter_all_appointments_by_status(status:str):
 
 def get_all_doctors_belong_to_department(department:object):
     """retrieves all Doctors belong to Department"""
-    doctors = Doctor.objects.select_related('specialization').filter(specialization__in=department.specializations.all())
-    return doctors
+    doctors = Doctor.objects.select_related('specialization').\
+        filter(specialization__in=department.specializations.all())
+    return doctors  
     
+    
+def filter_beds_by_availabilty(avilability:str):
+    """
+    filter all Beds by availability('available', 'occupied')
+
+    Args:
+        avilability (str): it should be one of these->('available', 'occupied')
+py manage.py runscript orm_script
+    """
+    return Bed.objects.filter(availability=avilability)
